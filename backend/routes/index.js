4653917d78ed209d91d69bd63420b645233342be
sfazli96 <sfazli96@gmail.com>
2022-12-14 13:00:00 -0800

    Added error-handling middlware for errors and debugging

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-router.use('/api', apiRouter);
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
 
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
