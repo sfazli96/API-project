c823c547b88371ca8659d6aaeba6da8b2082e50f
sfazli96 <sfazli96@gmail.com>
2023-01-12 18:41:02 -0800

    working on redux-store setup

---
+++ frontend/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
+import configureStore from './store';
+
+// Access to store and expose it to window
+const store = configureStore()
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+// define Root functional component that returns the App component wrapped in Redux Provider
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
