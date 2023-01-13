fa95ff80605fb29ebdb7661e88b7803dada181b3
sfazli96 <sfazli96@gmail.com>
2023-01-13 12:03:53 -0800

    sign-up thunk-action reducer working

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -22,6 +22,24 @@ export const restoreUser = () => async dispatch => {
     return response;
   };
 
+// thunk action creator (for sign up the user)
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user
+    const response = await csrfFetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password
+        })
+    })
+    const data = await response.json();
+    dispatch(setUser(data.user))
+    return response
+}
+
 // thunk action creator (login for the user)
 export const login = (user) => async (dispatch) => {
     const { credential, password } = user;
