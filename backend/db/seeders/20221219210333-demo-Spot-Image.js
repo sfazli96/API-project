cb842ad521d9c9eed611c9a3eb3976856d257b87
sfazli96 <sfazli96@gmail.com>
2023-01-18 15:04:46 -0800

    updated seeders in backend

--- backend/db/seeders/20221219210333-demo-Spot-Image.js
+++ backend/db/seeders/20221219210333-demo-Spot-Image.js
@@ -12,17 +12,17 @@ module.exports = {
    await queryInterface.bulkInsert(options, [
     {
       spotId: 1,
-      url: "image url",
+      url: "https://a0.muscache.com/im/pictures/68bc4aab-a736-417f-b7b6-ff2f1036bfc0.jpg?im_w=720",
       preview: true
     },
     {
       spotId: 2,
-      url: "image url 2",
-      preview: false
+      url: "https://a0.muscache.com/im/pictures/90c7a7eb-2182-4287-960d-5e31c94c5545.jpg?im_w=720",
+      preview: true
     },
     {
       spotId: 3,
-      url: "image url 3",
+      url: "https://a0.muscache.com/im/pictures/d3fff78e-831b-4189-ac41-d4c0d7fc50ef.jpg?im_w=720",
       preview: true
     },
    ], {})
