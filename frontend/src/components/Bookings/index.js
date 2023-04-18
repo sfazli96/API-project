3bd817110a0a2884e229eb54494c2d19e51ae95d
sfazli96 <sfazli96@gmail.com>
2023-04-18 07:45:39 -0700

    load bookings show on devtools

--- frontend/src/components/Bookings/index.js
+++ frontend/src/components/Bookings/index.js
@@ -2,24 +2,25 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import * as bookingsAction from "../../store/booking";
-// import { getAllBookings } from "../../store/booking";
+import { getAllBookings } from "../../store/booking";
 import Calendar from "react-calendar";
 import "./bookings.css";
 import 'react-calendar/dist/Calendar.css';
 import { addBookings } from "../../store/booking";
 
 const Bookings = () => {
-  const dispatch = useDispatch();
   const { id } = useParams();
+  const dispatch = useDispatch();
   const bookingDetail = useSelector((state) => state.booking.allBookings);
   const bookingsArr = Object.values(bookingDetail);
   const user = useSelector((state) => state.session.user);
   const spotDetail = useSelector((state) => state.spot.singleSpot);
+  const spotId = spotDetail.id
   const [showCalendar, setShowCalendar] = useState(false);
 
-  // useEffect(() => {
-  //   dispatch(getAllBookings(id));
-  // }, [dispatch, id]);
+  useEffect(() => {
+    dispatch(getAllBookings(spotId));
+  }, [dispatch, spotId]);
 
   const [selectedRange, setSelectedRange] = useState([null, null]);
 
@@ -47,24 +48,34 @@ const Bookings = () => {
     }
 
     const hasOverlappingDates = (startDate, endDate, bookingStart, bookingEnd) => {
-      return (startDate >= bookingStart && startDate <= bookingEnd) || (endDate >= bookingStart && endDate <= bookingEnd)
-    }
+      return (
+        (startDate >= bookingStart && startDate < bookingEnd) ||
+        (endDate > bookingStart && endDate <= bookingEnd) ||
+        (startDate <= bookingStart && endDate >= bookingEnd)
+      );
+    };
 
-    function conflicts(startDate, endDate, bookingsArr) {
-      for (const {startDate: bookingStart, endDate: bookingEnd} of bookingsArr) {
-        if (hasOverlappingDates(startDate, endDate, bookingStart, bookingEnd)) {
+
+    const conflicts = (startDate, endDate, bookingsArr) => {
+      for (const { startDate: bookingStart, endDate: bookingEnd } of bookingsArr) {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        const bookingStartObj = new Date(bookingStart);
+        const bookingEndObj = new Date(bookingEnd);
+        if (hasOverlappingDates(start, end, bookingStartObj, bookingEndObj)) {
           return true;
         }
       }
       return false;
-    }
+    };
 
-    if (conflicts) {
+
+    if (conflicts(startDate, endDate, bookingsArr)) {
       alert("The selected dates conflict with an existing booking");
       return;
     }
 
-    dispatch(addBookings({id, startDate, endDate, userId}));
+    dispatch(addBookings({spotId: id, startDate, endDate, userId}));
     setSelectedRange([null, null]);
   }
 
@@ -103,7 +114,7 @@ const Bookings = () => {
               />
               <p>Selected range: {selectedRange[0] ? selectedRange[0].toLocaleDateString() : 'None'} to {selectedRange[1] ? selectedRange[1].toLocaleDateString() : 'None'}</p>
             </div>
-            {/* <button onClick={handleCreateBooking}>Create Booking</button> */}
+            <button onClick={handleCreateBooking}>Create Booking</button>
           </>
         )
       ) : null}
